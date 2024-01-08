4da5755fe8b4ac4eac81f91e3c234e88d49d13de
yuvals2 <yuvals@qwilt.com>
2024-01-08 11:30:31 +0200

    add the project

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,33 @@
+import p1_img from './product_1.jpg'
+import p2_img from './product_2.jpg'
+import p3_img from './product_3.jpg'
+import p4_img from './product_4.jpg'
+
+let data_product = [
+    {
+        id: 1,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p1_img,
+        price: 50.00,
+    },
+    {
+        id: 2,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p2_img,
+        price: 140.00,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p3_img,
+        price: 85.00,
+    },
+    {
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p4_img,
+        price: 70.00,
+    }
+];
+
+export default data_product;
